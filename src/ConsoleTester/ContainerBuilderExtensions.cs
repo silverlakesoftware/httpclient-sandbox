48a9b793129991e8e1abe553d862dd24095ccdcc
Jamie da Silva <jdasilva@silverlakesoftware.com>
2023-10-18 00:53:01 -0400

    Refactor Docker project labels, Code cleanup

--- src/ConsoleTester/ContainerBuilderExtensions.cs
+++ src/ConsoleTester/ContainerBuilderExtensions.cs
@@ -4,6 +4,16 @@ namespace ConsoleTester;
 
 public static class ContainerBuilderExtensions
 {
+    // Used to create a project group of containers in Docker Desktop
+    const string DockerComposeProjectLabel = "com.docker.compose.project";
+    // Names the service for the project log
+    const string DockerComposeServiceLabel = "com.docker.compose.service";
+    // Required for project logs to appear, shouldn't matter unless your also trying to use compose with the same containers 
+    // https://github.com/docker/compose-cli/issues/935
+    const string DockerComposeConfigHashLabel = "com.docker.compose.config-hash";
+    // Required for project logs to appear
+    const string DockerComposeOneoffLabel = "com.docker.compose.oneoff";
+
     public static ContainerBuilder WithIpAddress(this ContainerBuilder cb, string ipAddress)
         => cb.WithCreateParameterModifier(pm => pm.NetworkingConfig.EndpointsConfig.Single().Value.IPAddress = ipAddress);
 
@@ -12,5 +22,14 @@ public static class ContainerBuilderExtensions
 
     public static ContainerBuilder WithDns(this ContainerBuilder cb, string ipAddress)
         => cb.WithCreateParameterModifier(pm => (pm.HostConfig.DNS ??= new List<string>()).Add(ipAddress));
+
+    public static ContainerBuilder WithProjectLabels(this ContainerBuilder cb, string projectName, string? serviceName = default, string? configHash  = default)
+    {
+        return cb
+            .WithLabel(DockerComposeProjectLabel, projectName)
+            .WithCreateParameterModifier(pm => pm.Labels.Add(DockerComposeServiceLabel, serviceName ?? pm.Name))
+            .WithLabel(DockerComposeConfigHashLabel, configHash ?? Guid.NewGuid().ToString())
+            .WithLabel(DockerComposeOneoffLabel, "False");
+    }
 
 }
--- src/ConsoleTester/Program.cs
+++ src/ConsoleTester/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text;
 using Docker.DotNet.Models;
 using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
 using DotNet.Testcontainers.Images;
 using DotNet.Testcontainers.Networks;
@@ -15,43 +14,21 @@ namespace ConsoleTester
 {
     internal class Program
     {
-        private const string HcsNetworkName = "hcs-demo-net";
-        private const string HcsProjectName = "hcs-demo";
+        const string HcsNetworkName = "hcs-demo-net";
+        const string DnsHostIpAddress = "172.20.1.2";
+        const string HcsProjectName = "hcs-demo";
+        const string HcsApiAppImage = "hcs-api-app";
 
         static async Task Main(string[] args)
         {
-            // var futureImage = new ImageFromDockerfileBuilder()
-            //     .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), @"src\MinimalService")
-            //     .WithDockerfile("Dockerfile")
-            //     .Build();
-            //
-            // await futureImage.CreateAsync().ConfigureAwait(false);
-
-
-            // await new ImageFromDockerfileBuilder()
-            //     .WithName("tc-minimalservice-image")
-            //     .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), @"src\MinimalService")
-            //     .WithDockerfile("Dockerfile")
-            //     .WithBuildArgument("RESOURCE_REAPER_SESSION_ID", ResourceReaper.DefaultSessionId.ToString("D")) // https://github.com/testcontainers/testcontainers-dotnet/issues/602.
-            //     .WithDeleteIfExists(false)
-            //     .Build()
-            //     .CreateAsync()
-            //     .ConfigureAwait(false);
-
-            // await CreateNetwork().ConfigureAwait(false);
-            //
-            // await CreateBind9Image().ConfigureAwait(false);
-            // await CreateBaseImage().ConfigureAwait(false);
-
-
             await Task.WhenAll(CreateBind9Image(), CreateBaseImage(), CreateHostAppImage(), CreateDnsTesterImage()).ConfigureAwait(false);
 
             await CreateNetwork().ConfigureAwait(false);
 
-            var bind9 = await StartBind9().ConfigureAwait(false);
-            await StartHost1().ConfigureAwait(false);
-            await StartHost2().ConfigureAwait(false);
-            await StartClient().ConfigureAwait(false);
+            var bind9 = await StartDnsHost().ConfigureAwait(false);
+            await StartApiApp1().ConfigureAwait(false);
+            await StartApiApp2().ConfigureAwait(false);
+            await StartClientApp().ConfigureAwait(false);
 
 
             var rawContents = await bind9.ReadFileAsync("/etc/bind/zones/db.silverlake.dev");
@@ -64,10 +41,6 @@ namespace ConsoleTester
 
             await bind9.ExecAsync(new[] { "rndc", "reload" });
 
-            // var container = await StartMinimalServiceAsync().ConfigureAwait(false);
-            //
-
-            // await container.DisposeAsync().ConfigureAwait(false);
         }
         
         static async Task<INetwork> CreateNetwork()
@@ -121,7 +94,7 @@ namespace ConsoleTester
             var image = new ImageFromDockerfileBuilder()
                 .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), ".")
                 .WithDockerfile("src/MinimalService/Dockerfile")
-                .WithName("hcs-host-app")
+                .WithName(HcsApiAppImage)
                 .WithCleanUp(false)
                 .Build();
 
@@ -139,29 +112,20 @@ namespace ConsoleTester
                 .WithCleanUp(false)
                 .Build();
 
+
             await image.CreateAsync().ConfigureAwait(false);
 
             return image;
         }
 
-        static async Task<IContainer> StartBind9()
+        static async Task<IContainer> StartDnsHost()
         {
             var container = new ContainerBuilder()
                 .WithName("hcs-dns-server")
                 .WithImage("hcs-dns-server")
-                .WithLabel("com.docker.compose.project", HcsProjectName)
-                .WithLabel("com.docker.compose.config-hash","1")
-                .WithLabel("com.docker.compose.service", "hcs-dns-server")
-                .WithLabel("com.docker.compose.container-number", "1")
-                .WithLabel("com.docker.compose.version", "2.21.0")
-                .WithLabel("com.docker.compose.oneoff", "False")
-                .WithCreateParameterModifier(pm => pm.AttachStdout = true)
-                .WithCreateParameterModifier(pm => pm.AttachStderr = true)
-                .WithAutoRemove(true)
+                .WithProjectLabels(HcsProjectName)
                 .WithNetwork(HcsNetworkName)
-                .WithIpAddress(HcsNetworkName, "172.20.1.2")
-                //.WithBindMount(@"E:\Sandbox\HttpClientSandbox\files\dns-example1\zones\", "/etc/bind/zones")
-                //.WithOutputConsumer()
+                .WithIpAddress(HcsNetworkName, DnsHostIpAddress)
                 .Build();
 
             await container.StartAsync().ConfigureAwait(false);
@@ -169,21 +133,14 @@ namespace ConsoleTester
             return container;
         }
 
-        static async Task<IContainer> StartHost1()
+        static async Task<IContainer> StartApiApp1()
         {
             var container = new ContainerBuilder()
-                .WithName("hcs-host1")
-                .WithImage("hcs-host-app")
-                .WithLabel("com.docker.compose.project", HcsProjectName)
-                .WithLabel("com.docker.compose.service", "hcs-host1")
-                .WithLabel("com.docker.compose.config-hash", "2")
-                .WithLabel("com.docker.compose.container-number", "2")
-                .WithLabel("com.docker.compose.oneoff", "False")
-                .WithCreateParameterModifier(pm => pm.AttachStdout = true)
-                .WithCreateParameterModifier(pm => pm.AttachStderr = true)
-                //.WithAutoRemove(true)
+                .WithName("hcs-api-1")
+                .WithImage(HcsApiAppImage)
+                .WithProjectLabels(HcsProjectName)
                 .WithNetwork(HcsNetworkName)
-                .WithDns("172.20.1.2")
+                .WithDns(DnsHostIpAddress)
                 .WithIpAddress(HcsNetworkName, "172.20.1.3")
                 .Build();
 
@@ -192,19 +149,14 @@ namespace ConsoleTester
             return container;
         }
 
-        static async Task<IContainer> StartHost2()
+        static async Task<IContainer> StartApiApp2()
         {
             var container = new ContainerBuilder()
-                .WithName("hcs-host2")
-                .WithImage("hcs-host-app")
-                .WithLabel("com.docker.compose.project", HcsProjectName)
-                .WithLabel("com.docker.compose.service", "hcs-host2")
-                .WithLabel("com.docker.compose.config-hash", "3")
-                .WithLabel("com.docker.compose.container-number", "3")
-                .WithLabel("com.docker.compose.oneoff", "False")
-                //.WithAutoRemove(true)
+                .WithName("hcs-api-2")
+                .WithImage(HcsApiAppImage)
+                .WithProjectLabels(HcsProjectName)
                 .WithNetwork(HcsNetworkName)
-                .WithDns("172.20.1.2")
+                .WithDns(DnsHostIpAddress)
                 .WithIpAddress(HcsNetworkName, "172.20.1.4")
                 .Build();
 
@@ -213,19 +165,14 @@ namespace ConsoleTester
             return container;
         }
 
-        static async Task<IContainer> StartClient()
+        static async Task<IContainer> StartClientApp()
         {
             var container = new ContainerBuilder()
                 .WithName("hcs-client")
                 .WithImage("hcs-dns-tester")
-                .WithLabel("com.docker.compose.project", HcsProjectName)
-                .WithLabel("com.docker.compose.service", "hcs-client")
-                .WithLabel("com.docker.compose.config-hash", "4")
-                .WithLabel("com.docker.compose.container-number", "4")
-                .WithLabel("com.docker.compose.oneoff", "False")
-                //.WithAutoRemove(true)
+                .WithProjectLabels(HcsProjectName)
                 .WithNetwork(HcsNetworkName)
-                .WithDns("172.20.1.2")
+                .WithDns(DnsHostIpAddress)
                 .WithIpAddress(HcsNetworkName, "172.20.1.5")
                 .Build();
 
